73d98b1aad2f6ba1749e6439a6a0cfc9bdbdc5f6
guillermocova <gacp87@gmail.com>
2015-06-29 18:03:15 -0500

    Header
    Eliminando basura del header

--- index.php
+++ index.php
@@ -27,12 +27,7 @@
     <nav class="navbar navbar-nav navbar-right navbar-inverse navbar-fixed-top">
       <div class="container" style="height:80px;">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
+          
           <a href="#" class="navbar-brand"><img class="img-responsive" src="img/logo-blanco.png"></img></a>
           
         </div>
